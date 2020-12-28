bbdfdfd004173a712638589729679d66f1ea8006
Sushil Kumar <johnklutz.sk@gmail.com>
2020-12-29 00:56:01 +0400

    format posts as cards

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(10);
+        $posts = Post::paginate(6);
         return view('posts.index', ['posts' => $posts]);
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,13 +7,40 @@
     </x-slot>
 
     <div class="container mx-auto px-10 py-10">
-        <ul>
+        <div class="flex flex-wrap ">
             @foreach($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', ['post' => $post]) }}">{{$post->title}}</a>
-            </li>
+            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <article class="overflow-hidden rounded-lg shadow-lg">
+                    <a href="#">
+                        <img alt="Placeholder" class="block h-auto w-full" src="https://picsum.photos/600/400/?random">
+                    </a>
+
+                    <header class="flex-wrap items-center justify-between leading-tight p-2 md:p-4">
+                        <p class="text-lg overflow-hidden">
+                            <a class="truncate max-h-full no-underline hover:underline text-black" href="{{ route('posts.show', ['post' => $post]) }}">
+                                {{ $post->title }}
+                            </a>
+                        </p>
+
+
+                    </header>
+
+                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <span class="flex items-center no-underline text-black" href="#">
+                            <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random">
+                            <p class="ml-2 text-sm">
+                                {{ $post->profile->user->name }}
+                            </p>
+                        </span>
+                        <p class="text-grey-darker text-sm">
+                            {{ date("F j, Y", strtotime($post->updated_at)) }}
+                        </p>
+                    </footer>
+                </article>
+            </div>
+
              @endforeach
-        </ul>
+        </div>
 
         <div>{{$posts->links()}}</div>
     </div>
