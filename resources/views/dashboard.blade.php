c347c36f60a6dae03913450baf4ecadf387fb31a
Sushil Kumar <johnklutz.sk@gmail.com>
2020-12-30 14:49:51 +0400

    update text in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,19 +13,19 @@
         <div class="grid grid-cols-3 gap-x-8 gap-y-4 h-80">
             <div class="p-6 mx-6 bg-white border-b border-gray-200">
                 <a href={{ route('profiles.show', Auth::user()->profile->id) }} class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent leading-5 text-gray-500 hover:text-blue-700 hover:border-red-500 transition duration-150 ease-in-out">
-                    View Profile
+                    View your profile
                 </a>
             </div>
 
             <div class="p-6 mx-6 bg-white">
                 <a href={{route('posts.index')}} class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent leading-5 text-gray-500 hover:text-blue-700 hover:border-red-500 transition duration-150 ease-in-out">
-                    View Posts
+                    View all posts
                 </a>
             </div>
 
             <div class="p-6 mx-6 bg-white border-b border-gray-200">
                 <a href={{route('posts.create')}} class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent leading-5 text-gray-500 hover:text-blue-700 hover:border-red-500 transition duration-150 ease-in-out">
-                    Create New Post
+                    Create new post
                 </a>
             </div>
         </div>
