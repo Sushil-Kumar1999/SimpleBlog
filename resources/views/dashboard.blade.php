1d17c128b7e30b27bac333aaab4fb76e75c81be5
Sushil Kumar <johnklutz.sk@gmail.com>
2021-01-01 16:36:06 +0400

    rename image icons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,21 +15,21 @@
                 <a href={{ route('profiles.show', Auth::user()->profile->id) }} class="inline-flex place self-center px-1 pt-1 border-b-2 border-transparent leading-5 text-green-700 hover:border-red-500 transition duration-150 ease-in-out">
                     View your profile
                 </a>
-                <img src="storage/images/default_profile_image.png" width="250" heigth="250" class="rounded-3xl place self-center" alt="profile image">
+                <img src="storage/images/profile_icon.png" width="300" heigth="300" class="rounded-3xl place self-center" alt="profile image">
             </div>
 
             <div class="flex flex-col p-6 mx-6 bg-blue-100 border-b border-gray-200 space-y-5 shadow-md hover:shadow-2xl rounded-2xl">
                 <a href={{route('posts.index')}} class="inline-flex place self-center px-1 pt-1 border-b-2 border-transparent leading-5 text-blue-700 hover:border-red-500 transition duration-150 ease-in-out">
                     View all posts
                 </a>
-                <img src="storage/images/view_posts.png" width="250" heigth="250" class="rounded-3xl place self-center" alt="profile image">
+                <img src="storage/images/view_posts_icon.png" width="250" heigth="250" class="rounded-3xl place self-center" alt="profile image">
             </div>
 
             <div class="flex flex-col p-6 mx-6 bg-yellow-100 border-b border-gray-200 space-y-5 shadow-md hover:shadow-2xl rounded-2xl">
                 <a href={{route('posts.create')}} class="inline-flex place self-center px-1 pt-1 border-b-2 border-transparent leading-5 text-yellow-700 hover:border-red-500 transition duration-150 ease-in-out">
                     Create new post
                 </a>
-                <img src="storage/images/create_new_post.png" width="250" heigth="250" class="rounded-3xl place self-center" alt="profile image">
+                <img src="storage/images/create_new_post_icon.png" width="250" heigth="250" class="rounded-3xl place self-center" alt="profile image">
             </div>
         </div>
     </div>
