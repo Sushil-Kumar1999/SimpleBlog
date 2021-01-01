126e6aaf7e61406e748568747a9e224c642e6605
Sushil Kumar <johnklutz.sk@gmail.com>
2021-01-01 13:49:35 +0400

    create default profile image when new user is registered

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Profile;
 use App\Models\Role;
+use App\Models\Image;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -48,11 +49,19 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
+        // create profile for new registered user
         $profile = new Profile();
         $profile->about = "This is my profile";
         $profile->last_active = now();
         $profile->user_id = $user->id;
         $profile->save();
+
+        // create default profile image
+        $image = new Image();
+        $image->storage_path = "public/images/default_profile_image.png";
+        $image->imageable_id = $profile->id;
+        $image->imageable_type = "App\Models\Profile";
+        $image->save();
 
         $role_id = Role::firstWhere('name', 'User')->id;
         $profile->roles()->attach($role_id);
